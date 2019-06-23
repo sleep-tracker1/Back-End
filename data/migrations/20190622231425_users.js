d60bb076d8ef79751273baa7a7985bff28b731db
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-06-23 13:25:45 -0500

    fixed user migrations

--- data/migrations/20190622231425_users.js
+++ data/migrations/20190622231425_users.js
@@ -2,11 +2,14 @@ exports.up = function(knex) {
   return knex.schema.createTable("users", table => {
     table.increments();
     table.timestamp("created_at").defaultTo(knex.fn.now());
-    table.string("username", 32);
-    table.string("password", 32);
+    table
+      .string("username", 32)
+      .unique()
+      .notNullable();
+    table.string("password", 32).notNullable();
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.raw("DROP TABLE IF EXISTS users CASCADE");
+  return knex.schema.raw("DROP TABLE IF EXISTS users");
 };
